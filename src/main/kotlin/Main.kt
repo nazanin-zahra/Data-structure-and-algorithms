6d896690aad158681c0d55f082ca7ea1f6556d3e
nazi <n.beigirad@gmail.com>
2022-08-21 21:13:00 +0430

    Data types & for & fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,75 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val niceJob = 1.0
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val nn = 1L
+    val n: Long = 1
+    fun family(firstName: String, surname: String) {
+
+        (0.rangeTo(100) step (5)).forEach() { i: Int ->
+        }
+
+        for (i in 0.rangeTo(100) step (5)) {
+        }
+        (0..100).forEach { number: Int ->
+
+        }
+        println()
+
+    }
+
+    val k: Char = 'j'
+    var h: Int
+    h = 5
+    h = 7
+    fun drive(): String? {
+        return null
+    }
+
+    val car: String? = "samand"
+    // car?.drive()
+
+    val drive: String? = drive()
+
+    println("$h \n $nn ")
+
+    val r = 3
+    val circle = PI * r.toDouble().pow(2)
+    println(circle)
+    //  var f:Int=null
+    var numOfClass: Int? = null
+//    if (numOfClass!!>5){
+    // }
+    val m = false
+    println(!m)
+
+    var name = "ati"
+    if (name!! == "ati") {
+        println("sisi")
+    } else if (name == "nazi") {
+        println("me")
+    } else {
+        println("un known")
+    }
+
+    when (name) {
+        "ati" -> println("sisi")
+        "nazi" -> println("me")
+        else -> {
+            println("un known")
+        }
+    }
+    var numbers = mutableListOf(4, 17, 15)
+    println(numbers[2])
+    println(" *${numbers.get(2)}*")
+    val score = 4
+    numbers[1] = score
+    println(numbers)
+    numbers.set(2, score)
+    println("****${numbers[2]}")
+    var x = 4
+    do {
+        x--
+    } while (x > 0)
+
+
 }
